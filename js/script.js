5efb27852626cc844af691125dc8297eb96fd33e
Nicolas Le Mellec <lemellec@live.fr>
2018-04-13 10:10:01 +0200

    Center les chartsJS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// CHARTJS LINE
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -18,10 +19,33 @@ var chart = new Chart(ctx, {
     options: {}
 });
 
+// CHARTJS RADAR
 var ctxRadar = document.getElementById('myChartRadar').getContext('2d');
 var chart = new Chart(ctxRadar, {
     // The type of chart we want to create
     type: 'radar',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
+// CHARTJS DOUGHNUT
+var ctxRadar = document.getElementById('myChartDoughnut').getContext('2d');
+var chart = new Chart(ctxRadar, {
+    // The type of chart we want to create
+    type: 'doughnut',
 
     // The data for our dataset
     data: {
